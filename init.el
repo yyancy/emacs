ca69bdcd5debdd14a71c82227cd34d3e16608b2e
yancy <yyancy517@gmail.com>
2022-05-12 22:17:34 +0800

    add some new configurations

--- init.el
+++ init.el
@@ -1,6 +1,12 @@
 ;; close welcome page
+
+;; You will most likely need to adjust this font size for your system!
+(defvar runemacs/default-font-size 140)
+
 (setq inhibit-startup-screen t)
 (setq apropos-sort-by-scores t)
+(set-fringe-mode 10)        ; Give some breathing room
+
 ;; change backup and cache directory
 (setq backup-directory-alist '(("." . "~/.cache/emacs/")))
 (setq auto-save-file-name-transforms
@@ -13,6 +19,15 @@
 (windmove-default-keybindings)
 (global-set-key [remap dabbrev-expand] 'hippie-expand)
 (show-paren-mode 1)
+
+(set-face-attribute 'default nil :height runemacs/default-font-size)
+
+;; Set the fixed pitch face
+(set-face-attribute 'fixed-pitch nil  :height 160)
+
+;; Set the variable pitch face
+(set-face-attribute 'variable-pitch nil  :height 200 :weight 'regular)
+
 
 ;; show buffer line numbers
 (column-number-mode)
@@ -136,6 +151,8 @@
   :init
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
   (setq evil-undo-system 'undo-fu)
 
 ;;  :hook (evil-mode . yancy/evil-hook)
@@ -154,6 +171,7 @@
 (use-package evil-collection
   :after evil
   :config
+  (setq evil-collection-magit-state 'normal)
   (evil-collection-init))
 (use-package undo-fu
   :ensure t)
@@ -217,6 +235,61 @@
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
+(require 'org-indent)
+
+(defun dw/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (auto-fill-mode 0)
+  (visual-line-mode 1)
+  (setq evil-auto-indent nil))
+
+(defun efs/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil  :weight 'regular :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+
+(use-package org
+  :hook (org-mode . efs/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾"
+	org-hide-emphasis-markers t)
+  (efs/org-font-setup))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun efs/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . efs/org-mode-visual-fill))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
